a2c462c6d8cbb894fe945a3423ce3e7e8eded273
thiago thg <thiagobonfim73@gmail.com>
2024-08-05 13:59:09 -0300

    alteração5

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -10,8 +10,9 @@ describe('Sign up', () => {
   const emailAddress = `${faker.datatype.uuid()}@${Cypress.env('MAILOSAUR_SERVER_ID')}.mailosaur.net`
   const password = Cypress.env('USER_PASSWORD')
 
-  it('successfully signs up using confirmation code sent via email', () => {
+  it('successfully signs up using confirmatiaon code sent via email', () => {
     cy.fillSignupFormAndSubmit(emailAddress, password)
+
 
     cy.contains('h1', 'Your Notes').should('be.visible')
     cy.contains('a', 'Create a new note').should('be.visible')
